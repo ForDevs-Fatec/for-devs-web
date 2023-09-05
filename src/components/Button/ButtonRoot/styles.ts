aa5e8652f9c52be398185cbc2ef0f42d3dcdd5b9
vbuarque <viniciusbuarque17@gmail.com>
2023-09-05 15:45:46 -0300

    style: Finalizando a estilização do componente de botão :okhand:

--- src/components/Button/ButtonRoot/styles.ts
+++ src/components/Button/ButtonRoot/styles.ts
@@ -1,5 +1,19 @@
 import {styled} from 'styled-components'
 
 export const ButtonPrimaryStyled = styled.button`
+    width: 100%;
+    padding: 1rem 1.5rem;
 
+    border: none;
+    border-radius: 0.375rem;
+
+    background-color: ${({theme}) => theme.colors.blue_500};
+
+    cursor: pointer;
+    
+    transition: all 0.1s;
+
+    &:hover {
+        background-color: ${({theme}) => theme.colors.blue_300};
+    }
 `;
