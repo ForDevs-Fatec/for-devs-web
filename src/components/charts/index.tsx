2a249b073ae8f409aa21d528a3f686cc4612f57b
vbuarque <viniciusbuarque17@gmail.com>
2023-11-01 21:02:49 -0300

    feat: finalizando a reacriação do grafico de barras

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -1,102 +1,55 @@
 import { ChartBarComponent } from "@/data/chartBarData";
+import { DonutsChartComponent } from "@/data/chartDonuts";
 
 export function DashboardCharts() {
     return (
-        <div className='flex justify-center'>
-            <div className='flex flex-col gap-4 h-full w-[1412px] pb-6'>
-                <div className='flex flex-wrap items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
+        <div className='flex flex-col gap-5 justify-center'>
+            <div className='flex gap-5 h-[97px] w-full'>
+                <div className='flex items-center justify-center gap-5 w-full'>
+                    <div className='flex flex-col gap-4 h-[97px] w-[25%] px-6 py-4 bg-[#282828] rounded-[1.25rem] hover:scale-105 hover:cursor-pointer'>
                         <h1 className='text-white font-semibold'>
                             Bar Chart
                         </h1>
-                        <ChartBarComponent />
+                        
                     </div>
 
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
+                    <div className='flex flex-col gap-4 h-[97px] w-[25%] px-6 py-4 bg-[#282828] rounded-[1.25rem] hover:scale-105 hover:cursor-pointer'>
                         <h1 className='text-white font-semibold'>
                             Bar Chart
                         </h1>
-                        <ChartBarComponent />
-                    </div>
-                </div>
-
-                <div className='flex items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
+                        
                     </div>
 
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
+                    <div className='flex flex-col gap-4 h-[97px] w-[25%] px-6 py-4 bg-[#282828] rounded-[1.25rem] hover:scale-105 hover:cursor-pointer'>
                         <h1 className='text-white font-semibold'>
                             Bar Chart
                         </h1>
-                        <ChartBarComponent />
+                        
+                    </div>
+
+                    <div className='flex flex-col gap-4 h-[97px] w-[25%] px-6 py-4 bg-[#282828] rounded-[1.25rem] hover:scale-105 hover:cursor-pointer'>
+                        <h1 className='text-white font-semibold'>
+                            Bar Chart
+                        </h1>
+                        
                     </div>
                 </div>
+            </div>
 
-                <div className='flex items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
+            <div className='flex flex-col gap-5 h-full w-full'>
+                <div className='flex items-center justify-center gap-4'>
+                    <div className='flex flex-col gap-4 h-[345px] w-[65%] p-6 bg-[#282828] rounded-[1.25rem]'>
                         <h1 className='text-white font-semibold'>
-                            Bar Chart
+                            Distribuição de sentimento x tema
                         </h1>
                         <ChartBarComponent />
                     </div>
 
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
+                    <div className='flex flex-col gap-4 h-[345px] w-[35%] p-6 bg-[#282828] rounded-[1.25rem]'>
                         <h1 className='text-white font-semibold'>
                             Bar Chart
                         </h1>
-                        <ChartBarComponent />
-                    </div>
-                </div>
-
-                <div className='flex items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
-                    </div>
-
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
-                    </div>
-                </div>
-
-                <div className='flex items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
-                    </div>
-
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
-                    </div>
-                </div>
-
-                <div className='flex items-center justify-center gap-4 w-full'>
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
-                    </div>
-
-                    <div className='flex flex-col gap-4 h-[260px] w-[520px] p-6 bg-[#282828] rounded-2xl'>
-                        <h1 className='text-white font-semibold'>
-                            Bar Chart
-                        </h1>
-                        <ChartBarComponent />
+                        <DonutsChartComponent />
                     </div>
                 </div>
             </div>
--- src/pages/metrics/index.tsx
+++ src/pages/metrics/index.tsx
@@ -1,187 +1,237 @@
 import { HeaderComponent } from "@/components/headerComponent";
+import { AlarmClock, Percent } from "lucide-react";
 import Chart from 'react-apexcharts';
+import ClockGif from '@/assets/pablita-clock-1.gif';
 
 const options: ApexCharts.ApexOptions = {
-    chart: {
-        type: 'area',
+  chart: {
+    type: 'area',
+  },
+  dataLabels: {
+    enabled: false
+  },
+  series: [
+    {
+      name: 'Tokenização',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#F87171'
     },
-    dataLabels: {
-        enabled: false
+    {
+      name: 'Pré processamento',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#FBBF24'
     },
-    series: [
-        {
-          name: 'Tokenização',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#F87171'
-        },
-        {
-          name: 'Pré processamento',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#FBBF24'
-        },
-        {
-          name: 'Stopword',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#34D399'
-        },
-        {
-          name: 'Analise de sentimentos',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#60A5FA'
-        },
-        {
-          name: 'Classificação de tema',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#f8a581'
-        },
-        {
-          name: 'Pesquisa PLN',
-          data: generateRandomDataAndAverage(7).data,
-          color: '#fa8be4'
-        },
+    {
+      name: 'Stopword',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#34D399'
+    },
+    {
+      name: 'Analise de sentimentos',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#60A5FA'
+    },
+    {
+      name: 'Classificação de tema',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#f8a581'
+    },
+    {
+      name: 'Pesquisa PLN',
+      data: generateRandomDataAndAverage(7).data,
+      color: '#fa8be4'
+    },
+  ],
+  fill: {
+    type: "solid",
+    opacity: 0.8,
+    colors: ['#F87171', '#FBBF24', '#34D399', '#60A5FA', '#f8a581', '#fa8be4'],
+  },
+  stroke: {
+    curve: 'smooth',
+    colors: ['#F87171', '#FBBF24', '#34D399', '#60A5FA', '#f8a581', '#fa8be4'],
+  },
+  xaxis: {
+    categories:
+      [
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-feira',
+        'Sexta-feira',
+        'Sabado',
+        'Domingo'
       ],
-    fill: {
-        type: "solid",
-        opacity: 0.8,
-        colors: ['#F87171', '#FBBF24', '#34D399', '#60A5FA', '#f8a581', '#fa8be4'],
+    labels: {
+      style: {
+        colors: '#FFFFFF',
+        fontSize: '12px',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 400,
+        cssClass: 'apexcharts-xaxis-label',
+      },
+    }
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: '#FFFFFF',
+        fontSize: '12px',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 400,
+        cssClass: 'apexcharts-xaxis-label',
+      },
+    }
+  },
+  tooltip: {
+    theme: 'dark',
+    y: {
+      formatter: function (val: number) {
+        return val + "s"
+      }
+    }
+  },
+  legend: {
+    labels: {
+      colors: '#FFFFFF',
     },
-    stroke: {
-        curve: 'smooth',
-        colors: ['#F87171', '#FBBF24', '#34D399', '#60A5FA', '#f8a581', '#fa8be4'],
-    },
-    xaxis: {
-        categories:
-            [
-                'Segunda-feira',
-                'Terça-feira',
-                'Quarta-feira',
-                'Quinta-feira',
-                'Sexta-feira',
-                'Sabado',
-                'Domingo'
-            ],
-        labels: {
-            style: {
-                colors: '#FFFFFF',
-                fontSize: '12px',
-                fontFamily: 'Roboto, sans-serif',
-                fontWeight: 400,
-                cssClass: 'apexcharts-xaxis-label',
-            },
-        }
-    },
-    yaxis: {
-        labels: {
-            style: {
-                colors: '#FFFFFF',
-                fontSize: '12px',
-                fontFamily: 'Roboto, sans-serif',
-                fontWeight: 400,
-                cssClass: 'apexcharts-xaxis-label',
-            },
-        }
-    },
-    tooltip: {
-        theme: 'dark',
-        y: {
-            formatter: function (val: number) {
-                return val + "s"
-            }
-        }
-    },
-    legend: {
-        labels: {
-            colors: '#FFFFFF',
-        },
-        position: 'right',
-        horizontalAlign: 'center',
-    },
+    position: 'right',
+    horizontalAlign: 'center',
+  },
 }
 
 function generateRandomDataAndAverage(numPoints = 7) {
-    const data = [];
-    let sum = 0;
+  const data = [];
+  let sum = 0;
 
-    for (let i = 0; i < numPoints; i++) {
-        const randomValue = Math.floor(Math.random() * 100);
-        data.push(randomValue);
-        sum += randomValue;
-    }
+  for (let i = 0; i < numPoints; i++) {
+    const randomValue = Math.floor(Math.random() * 100);
+    data.push(randomValue);
+    sum += randomValue;
+  }
 
-    const average = sum / numPoints;
-    return { data, average };
+  const average = sum / numPoints;
+  return { data, average };
 }
 
 export function MetricsPage() {
-    const { data: tokenizacaoData, average: tokenizacaoAverage } = generateRandomDataAndAverage(7);
-    const { data: preprocessamentoData, average: preprocessamentoAverage } = generateRandomDataAndAverage(7);
-    const { data: stopwordData, average: stopwordAverage } = generateRandomDataAndAverage(7);
-    const { data: analiseSentimentosData, average: analiseSentimentosAverage } = generateRandomDataAndAverage(7);
-    const { data: classificacaoTemaData, average: classificacaoTemaAverage } = generateRandomDataAndAverage(7);
-    const { data: pesquisaPLNData, average: pesquisaPLNAverage } = generateRandomDataAndAverage(7);
-  
-    return (
-      <div className='h-screen w-full p-4 '>
-        <HeaderComponent />
-  
-        <div className='flex flex-col gap-4 h-[90%] mt-8 mx-28'>
-          <div className='bg-zinc-800 w-full h-40 p-6 rounded-md shadow-lg'>
-            <div>
-              <div className='flex gap-4'>
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Pré processamento</span>
-                    <span className='text-3xl font-bold text-white'>{`Média ${preprocessamentoAverage.toFixed(1)}s`}</span>
+  const { average: tokenizacaoAverage } = generateRandomDataAndAverage(7);
+  const { average: preprocessamentoAverage } = generateRandomDataAndAverage(7);
+  const { average: stopwordAverage } = generateRandomDataAndAverage(7);
+  const { average: analiseSentimentosAverage } = generateRandomDataAndAverage(7);
+  const { average: classificacaoTemaAverage } = generateRandomDataAndAverage(7);
+  const { average: pesquisaPLNAverage } = generateRandomDataAndAverage(7);
+
+  return (
+
+    <div className='p-4'>
+      <HeaderComponent />
+      <div className="flex justify-center items-center">
+        <div className='flex flex-col justify-start gap-5 w-[71.875rem] h-screen'>
+          <div className='flex flex-col items-center justify-center gap-5'>
+            <div className='flex items-center justify-center gap-5 w-full'>
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
                 </div>
-  
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Stopword</span>
-                    <span className='text-3xl font-bold text-white'>{`Média ${stopwordAverage.toFixed(1)}s`}</span>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Pré processamento
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${preprocessamentoAverage.toFixed(1)}s`}</span>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
                 </div>
-  
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Tokenização</span>
-                    <span className='text-3xl font-bold text-white'>{`Média ${tokenizacaoAverage.toFixed(1)}s`}</span>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Stopword
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${stopwordAverage.toFixed(1)}s`}</span>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
                 </div>
-  
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Analise de sentimentos</span>
-                    <span className='text-3xl font-bold text-white'>{`Média ${analiseSentimentosAverage.toFixed(1)}s`}</span>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Tokenização
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${tokenizacaoAverage.toFixed(1)}s`}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex items-center justify-center gap-5 w-full'>
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
                 </div>
-  
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Classificação de tema</span>
-                    <span className='text-3xl font-bold text-white'>{`Média ${classificacaoTemaAverage.toFixed(1)}s`}</span>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Analise de sentimentos
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${analiseSentimentosAverage.toFixed(1)}s`}</span>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
                 </div>
-  
-                <div className='bg-zinc-700 w-[25%] h-28 rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:cursor-pointer'>
-                  <div className='flex flex-col justify-center items-center gap-2 h-full'>
-                    <span className='text-zinc-400'>Pesquisa PLN</span>
-                    <span className='text-4xl font-bold text-white'>{`Média ${pesquisaPLNAverage.toFixed(1)}s`}</span>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Classificação de tema
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${classificacaoTemaAverage.toFixed(1)}s`}</span>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between p-5 h-full bg-[#282828] w-[33%] rounded-xl hover:scale-105 hover:cursor-pointer transition-all '>
+                <div className='w-[30%]'>
+                  <div className='flex items-center justify-center rounded-full bg-zinc-700 w-14 h-14'>
+                    <AlarmClock className='text-white' />
                   </div>
+                </div>
+                <div className='flex flex-col items-end w-[70%]'>
+                  <h1 className='text-zinc-400 text-sm font-thin'>
+                    Pesquisa PLN
+                  </h1>
+                  <span className='text-2xl text-white font-semibold'>{`Média ${pesquisaPLNAverage.toFixed(1)}s`}</span>
                 </div>
               </div>
             </div>
           </div>
-  
-          <div className='bg-zinc-800 w-full h-full p-8 rounded-md shadow-lg'>
+
+          <div className='flex flex-col h-[65%] p-6 bg-[#282828] rounded-[1.25rem]'>
+            <h1 className='text-white text-sm font-thin'>
+              Bar Chart
+            </h1>
+
             <Chart
-              options={options}
-              series={options.series}
-              width='100%'
-              height='100%'
-            />
+                options={options}
+                series={options.series}
+                width='100%'
+                height='100%'
+              />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
